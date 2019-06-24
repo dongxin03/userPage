6de4c7b5c52ce167bd43f2967f10942ad6b103fb
董新 <dongxin@huohua.cn>
2019-06-24 17:01:29 +0800

    new Page

---
+++ user-page/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p style={{fontSize:'50px',textAlign:'center',marginTop:'300px'}}>
+        这是一个新的页面
+      </p>
+    </div>
+  );
+}
+
+export default App;
